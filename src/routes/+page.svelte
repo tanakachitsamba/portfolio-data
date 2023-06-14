f2e4983c5dd70aa6c64f943948e756e4f8f56987
Tanaka <alongthejourneyahead@gmail.com>
2023-06-14 14:53:41 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,108 @@
+<script>
+</script>
+<style>
+    .nav-link {
+        color: black;
+        --bs-nav-link-color: ;
+        
+    }
+
+    .aa {
+        font-size: 25px;
+    }
+
+    .bb {
+        width: 83%;
+        margin: auto;
+        padding-top: 20px;
+        
+    }
+
+    .cc {
+        --bs-card-border-radius: 0;
+    }
+
+    .dd {
+        padding-top: 10px;
+        
+    }
+
+    .ee {
+        font-weight: 500;
+        font-size: 20px;
+    }
+
+    .ff {
+        font-size: 30px;
+        padding-top: 100px;
+        padding-bottom: 100px;
+        
+    }
+
+    main {
+        background-color: antiquewhite;
+    }
+</style>
+
+<main>
+    <nav class="navbar navbar-expand-lg">
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>-->
+            <div class="container aa">
+              <a class="navbar-brand aa" href="/">Tanaka Chitsamba</a>
+              <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">My Work</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">About Me</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Contact</a>
+                  </li>
+                  <!-- Add more navigation items as needed -->
+                </ul>
+              </div>
+            </div>
+    </nav>
+
+    <div class="bb">
+        <div class="container">
+            <div class="ff">Hey! I'm Tanaka. I am an AI researcher, based in Nottingham, UK.</div>
+            <span class="ee ff">My Work</span>     
+            <div class="row dd">
+              <div class="col-md-4">
+                <!-- First item -->
+                <div class="cc">
+                  <div class="card-body">
+                    <h5 class="card-title">Bias in Stable Diffusion Models</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <!-- Second item -->
+                <div class="cc">
+                  <div class="card-body">
+                    <h5 class="card-title">Item 2</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <!-- Third item -->
+                <div class="cc">
+                  <div class="card-body">
+                    <h5 class="card-title">Item 3</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+</main>
+
